2fb6f8006984af8b3222bf30cb86fa53f644b980
Antwane Mason <git@aimai.simplelogin.com>
2025-04-05 19:57:24 -0400

    home-manager: Added additional git conf & aliases

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -3,5 +3,40 @@
     enable = true;
     userName = "Antwane Mason";
     userEmail = "git@aimai.simplelogin.com";
+    aliases = {
+      lol = "log --oneline --decorate";
+      graph = "log --oneline --decorate --graph";
+      rel = "reflog --date=iso";
+      ref = "reflog --date=iso --pretty";
+      di = "diff --word-diff";
+      dc = "diff --word-diff --cached";
+      bla = "blame -C -C -C"; # does a better job in determining source of change at the expense of speed
+      ca = "commit --amend --no-edit";
+      ci = "commit --verbose";
+      fp = "push --force-with-lease";
+      fpstack = ''        !git log --decorate=short --pretty='format:%D' origin/master.. |
+                          sed 's/, /\\n/g; s/HEAD -> //' |
+                          grep -Ev '/|^$' | xargs git push --force-with-lease origin'';
+      prb = "pull --rebase";
+      rc = "rebase --continue";
+      rb = ''        !f() { if [ $# -eq 0 ]; then set -- origin/master; git fetch origin master; fi && 
+                     git rebase "$@"; }; f'';
+      ri = ''        !f() { if [ $# -eq 0 ]; then set -- origin/main; fi; 
+                     git rebase --interactive --keep-base "$@"; }; f'';
+      st = "status --short";
+      sw = ''        !f() { if [ $# -gt 0 ]; then git switch "$@"; else git branch --sort=-committerdate | 
+                     fzf | xargs git switch; fi; }; f'';
+    };
+    extraConfig = {
+      rerere.enable = true; # record resolved conflicts to reuse
+      merge.conflictstyle = "diff3"; # give context of original in addition to conflicting changes
+      core.eol.text = "auto"; # auto detect end-of-line
+      pull.ff = "only"; # only do fast forward merges on pull
+      rebase.updateRefs = true; # enables rebasing whole branch stack with one command
+      grep = {
+        lineNumber = true;
+        patternType = "perl"; # use perl style regex
+      };
+    };
   };
 }
