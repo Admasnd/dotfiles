b632d01e4c5c0f07e51dbd69e6120a4afd908e99
Antwane Mason <git@aimai.simplelogin.com>
2025-04-05 15:03:27 -0400

    Merged flakes & generated flake lock

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "Flake for dotfiles including NixOS and home-manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nvf = {
+      url = "github:notashelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      nixframe = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./nixos/nixframe/configuration.nix
+          inputs.nixos-hardware.nixosModules.framework-11th-gen-intel
+          inputs.sops-nix.nixosModules.sops
+          ({config, ...}: {
+            system.configurationRevision = self.rev or "dirty";
+            sops.defaultSopsFile = ./secrets/secrets.yaml;
+            sops.defaultSopsFormat = "yaml";
+            sops.age.keyFile = "/home/antwane/.config/sops/age/keys.txt";
+            sops.secrets.borgbase_pass = {};
+          })
+        ];
+        specialArgs = {inherit inputs;};
+      };
+    };
+    homeConfigurations = {
+      antwane = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          config.allowUnfree = true;
+          inherit system;
+        };
+
+        modules = [
+          inputs.nvf.homeManagerModules.default
+          ./home-manager/home.nix
+        ];
+        extraSpecialArgs = inputs;
+      };
+    };
+  };
+}
