8e43a54f8dbb1ff29256e83f2978f2e6fb212aac
Antwane Mason <git@aimai.simplelogin.com>
2025-06-27 18:19:32 -0400

    nixos/nixframe: Added Power Mgmt Optimizations

--- nixos/nixframe/configuration.nix
+++ nixos/nixframe/configuration.nix
@@ -8,6 +8,44 @@
     ./base.nix
   ];
 
+  # prevent overheating
+  services.thermald.enable = true;
+
+  systemd.sleep.extraConfig = ''
+    HibernateDelaySec=60min
+  '';
+
+  services.logind.lidSwitch = "suspend-then-hibernate";
+
+  services.tlp = {
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "performance";
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+      #Optional helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+    };
+  };
+
+  boot.kernelParams = [
+    "mem_sleep_default=deep" # use low-leakage S3
+    "intel_idle.max_cstate=9" # let cores reach deepest C-state
+    "pcie_aspm.policy=powersupersave"
+  ];
+
+  services.logind.extraConfig = ''
+    [Sleep]
+    SuspendState=deep
+  '';
+
+  powerManagement.powertop.enable = true;
+
   # Use the systemd-boot EFI boot loader.
   # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
