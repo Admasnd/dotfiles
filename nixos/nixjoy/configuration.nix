bfa38e869479109f2d1f7f8a7f0a9eb3cab8bccc
Antwane Mason <git@aimai.simplelogin.com>
2025-07-04 16:27:04 -0400

    Fixed Commented Out Sunshine Firewall Whitelisting

--- nixos/nixjoy/configuration.nix
+++ nixos/nixjoy/configuration.nix
@@ -97,8 +97,9 @@ in {
   # #   ];
   # # };
   # #
-  # networking.firewall.interfaces."tailscale0".allowedTCPPorts = [47984 47989 47990 48010];
-  # networking.firewall.interfaces."tailscale0".allowedUDPPorts = [47998 47999 48000 48002 48010];
+  # This is needed for sunshine service
+  networking.firewall.interfaces."tailscale0".allowedTCPPorts = [47984 47989 47990 48010];
+  networking.firewall.interfaces."tailscale0".allowedUDPPorts = [47998 47999 48000 48002 48010];
   #
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
