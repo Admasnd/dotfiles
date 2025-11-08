7a98e52483abb0c39875b8989b4cfbdfab712360
Antwane Mason <git@aimai.simplelogin.com>
2025-11-07 23:19:58 -0500

    Added upgrade script and nixos checks
    - have nix flake check build host systems
    - upgrade script to be used with auto upgrade service

--- flake.nix
+++ flake.nix
@@ -38,6 +38,7 @@
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       nixosConfigurations = {
@@ -92,5 +93,40 @@
             putty
           ];
         };
+      checks.${system} = nixpkgs.lib.genAttrs (builtins.attrNames self.nixosConfigurations) (
+        host: self.nixosConfigurations.${host}.config.system.build.toplevel
+      );
+      packages.${system} = {
+        upgrade = pkgs.writeShellApplication {
+          name = "nixos-upgrade";
+
+          runtimeInputs = with pkgs; [
+            git
+            mktemp
+          ];
+          text = ''
+            FLAKE_LOCK=$(mktemp)
+            REPO_PATH=/home/antwane/dev/dotfiles
+
+            cleanup() {
+              rm -f "$FLAKE_LOCK"
+            }
+            trap cleanup EXIT INT TERM
+
+            cd "$REPO_PATH"
+            nix flake update --output-lock-file "$FLAKE_LOCK"
+            if cmp --silent -- "$FLAKE_LOCK" "flake.lock"; then
+              exit 0
+            fi
+            nix flake check --reference-lock-file "$FLAKE_LOCK"
+            chown "$(stat -c '%U:%G' flake.lock)" "$FLAKE_LOCK"
+            chmod "$(stat -c '%a' flake.lock)" "$FLAKE_LOCK"
+            mv "$FLAKE_LOCK" flake.lock
+            git add flake.lock
+            git commit --author "root <root@localhost>" -m "flake: update"
+            nixos-rebuild switch --flake .
+          '';
+        };
+      };
     };
 }
