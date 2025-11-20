24a5a580c34735f207785beeb4540297ed850e4c
Antwane Mason <git@aimai.simplelogin.com>
2025-11-20 16:07:49 -0500

    feat: Migrate iso host to aspect

--- modules/hosts/hosts.nix
+++ modules/hosts/hosts.nix
@@ -15,29 +15,22 @@
     includes = [ den.aspects.tailscale ];
     nixos = {
       imports = [
-        ../hosts/nixframe/configuration.nix
+        ../../hosts/nixframe/configuration.nix
         inputs.private-dotfiles.nixosModules.tailscale
         inputs.private-dotfiles.nixosModules.backup
         inputs.private-dotfiles.nixosModules.pam
         inputs.nixos-hardware.nixosModules.framework-11th-gen-intel
         inputs.disko.nixosModules.disko
-        ../hosts/nixframe/nixframe-disko.nix
+        ../../hosts/nixframe/nixframe-disko.nix
         inputs.home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.antwane = ../hosts/nixframe/home.nix;
+          home-manager.users.antwane = ../../hosts/nixframe/home.nix;
           home-manager.extraSpecialArgs = { inherit inputs; };
         }
       ];
     };
-  };
-
-  den.aspects.iso.nixos = {
-    imports = [
-      "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix"
-      ../hosts/iso/configuration.nix
-    ];
   };
 
   # Adding checks to build each host
