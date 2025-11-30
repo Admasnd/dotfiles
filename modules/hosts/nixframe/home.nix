e743ddae746cf653a6f2e864106f5c49bd3aef46
Antwane Mason <git@aimai.simplelogin.com>
2025-11-30 02:51:51 -0500

    refactor(nixframe): remove dup unfree override
    Already have an unfree override for steam in gaming aspect.
    
    index ee45beeff4..b565d74eba 100644
    --- a/modules/hosts/nixframe/home.nix
    +++ b/modules/hosts/nixframe/home.nix
    @@ -42,13 +42,6 @@
             home.username = "antwane";
             home.homeDirectory = "/home/antwane";
     
    -        nixpkgs.config.allowUnfreePredicate =
    -          pkg:
    -          builtins.elem (lib.getName pkg) [
    -            "steam-unwrapped"
    -            "steam"
    -          ];
    -
             home.packages = with pkgs; [
               # security tools
               bitwarden-desktop

--- modules/hosts/nixframe/home.nix
+++ modules/hosts/nixframe/home.nix
@@ -42,13 +42,6 @@
         home.username = "antwane";
         home.homeDirectory = "/home/antwane";
 
-        nixpkgs.config.allowUnfreePredicate =
-          pkg:
-          builtins.elem (lib.getName pkg) [
-            "steam-unwrapped"
-            "steam"
-          ];
-
         home.packages = with pkgs; [
           # security tools
           bitwarden-desktop
