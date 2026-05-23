a2a9555d0df5b5c83e2ae91757723fb4f1cccf92
Antwane Mason <git@aimai.simplelogin.com>
2026-05-22 20:13:08 -0400

    fix(papis): resolve build issues for papis from flake update
    change papis version from nixpkgs-stable to nixpkgs.

--- modules/papis/default.nix
+++ modules/papis/default.nix
@@ -2,7 +2,7 @@
 {
   flake.modules.homeManager.papis = {
     home.packages = [
-      inputs.nixpkgs-stable.legacyPackages.x86_64-linux.papis
+      inputs.nixpkgs.legacyPackages.x86_64-linux.papis
     ];
     xdg.configFile."papis/config".source = ./config;
   };
